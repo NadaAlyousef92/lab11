60d2dfb729e5a57ac756d06824cbd12283a4a1d4
NadaAlyousef92 <nada.alyousef92@gmail.com>
2021-02-23 03:35:44 +0200

    test2

--- js/app.js
+++ js/app.js
@@ -1,89 +1,177 @@
-const Imgstore = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'pet', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicom', 'waterCan', 'wineGlass', 'usb'];
+"use strict";
+
+const Imgstore = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicom', 'water-can', 'wine-glass', 'usb'];
 
 
 
 const leftImage = document.getElementById('left-image');
 const rightImage = document.getElementById('right-image');
-const centerImg = document.getElementById('center-img');
-console.log(leftImage);
-const imagesSection = document.getElementById('images-section');
+const centerImage = document.getElementById('center-image');
+
+
+const imagesSection = document.getElementById('imges-section');
 
 
 
-function imgMall(ImgName, path) {
-    this.ImgName = ImgName;
-    this.path = path;
-    this.votes = 0;
+function ImgMall(name) {
+    this.name = name;
+    this.path = `./img/${name}.jpg`;
+    this.vote = 0;
     this.views = 0;
 
 
-    Img.all(this);
+    ImgMall.all.push(this);
 }
 
-function randomNumber(min, max) {
+
+
+
+
+
+
+ImgMall.all = [];
+
+for (let i = 0; i < Imgstore.length; i++) {
+    new ImgMall(Imgstore[i]);
+}
+
+
+function render() {
+    const leftIndex = RandomNumber(0, ImgMall.all.length - 1);
+    leftImage.src = ImgMall.all[leftIndex].path;
+    leftImage.title = ImgMall.all[leftIndex].name;
+    leftImage.alt = ImgMall.all[leftIndex].name;
+
+    const centerIndex = RandomNumber(0, ImgMall.all.length - 1);
+    centerImage.src = ImgMall.all[centerIndex].path;
+    centerImage.title = ImgMall.all[centerIndex].name;
+    centerImage.alt = ImgMall.all[centerIndex].name;
+
+    const rightIndex = RandomNumber(0, ImgMall.all.length - 1);
+    rightImage.src = ImgMall.all[rightIndex].path;
+    rightImage.title = ImgMall.all[rightIndex].name;
+    rightImage.alt = ImgMall.all[rightIndex].name;
+}
+
+imagesSection.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    if (event.target.id !== 'images-section') {
+        for (let i = 0; i < ImgMall.all.length; i++) {
+            if (ImgMall.all[i].name === event.target.title) {
+                ImgMall.all[i].votes++;
+
+            }
+        }
+        render();
+
+    }
+}
+
+
+
+
+
+
+
+function RandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-Img.all = [];
+render();
+
+const Imgarr = [];
+
+const Maxvotes = 25;
+const votesSum = 0;
+
+const Threeimg = 3;
 
 
 
-
-console.table(Img.all);
-
-function render() {
-    const leftIndex = randomNumber(0, Goat.all.length - 1);
-    console.log('LEFT', leftIndex, Img.all[leftIndex].path);
-    leftImage.src = Img.all[leftIndex].Imgstore;
-    leftImage.title = Img.all[leftIndex].Imgstore;
-    leftImage.alt = Img.all[leftIndex].Imgstore;
-
-    const centerIndex = randomNumber(0, Img.all.length - 1);
-    console.log('LEFT', centerIndex, Img.all[centerIndex].path);
-    centerImage.src = Img.all[centerIndex].path;
-    centerImage.title = Img.all[centerIndex].Imgstore;
-    centerImage.alt = Img.all[centerIndex].Imgstore;
-
-    const rightIndex = randomNumber(0, Img.all.length - 1);
-    console.log('Right', rightIndex);
-    rightImage.src = Img.all[rightIndex].path;
-    rightImage.title = Img.all[rightIndex].Imgstore;
-    rightImage.alt = Img.all[rightIndex].Imgstore;
+function renderPics() {
+    while (ImgContainer.firstChild) {
+        ImgContainer.removeChild(ImgContainer.firstChild);
+    }
+    for (let i = 0; i < Threeimg; i++) {
+        let newEl = RandomNumber();
+    }
 }
-imagesSection.addEventListener('click', handleClick);
 
-
-
-
-function handleClick(event) {
-    [rightIndex, centerIndex, leftIndex];
-
-
-    let attempts = 25;
-    let vote = 0;
-    let imges = 3;
-
-
-
-    if (attempts < vote) {
-        if (event.target.id === rightIndex || event.target.id === centerIndex || event.target.id === leftIndex) {
-
-            for (let i = 0; i < Imgstore.all.length; i++) {
-                if (Imgstore.all[i].name === event.target.title) {
-                    console.log(Imgstore.all[i].ImgName, event.target.title);
-                    Imgstore.all[i].vote++;
-                }
-
-
-
-
-                attempts++
-
-                userInput = prompt('please try again : ' + attempts + ' remaining for you');
-            }
+function Counter(votes) {
+    votesSum++;
+    for (let i = 0; i < ImgMall.all.length; i++) {
+        if (votes.target.title === Imgarr[i].name) {
+            Imgarr[i].Maxvotes++;
         }
+
     }
 
+    if (votesSum >= Maxvotes) {
+        Container.removeEventListener('click', Counter);
+
+
+    }
+
+    renderpics();
 }
-render.handleClick();
-render.userInput();
+
+function clicks() {
+
+    select.removeEventListener('click', clicks);
+
+    const list = document.getElementById('Imgresults');
+    const ul = document.createElement('ul');
+    list.appendChild(ul);
+    renderElement('h2', resultsEl, '');
+    for (let i = 0; i < Imgarr.length; i++) {
+        if (Imgarr[i].Maxvotes > 0) {
+            const display = `${Imgarr[i].name} its ${Imgarr[i].Maxvotes} votes, and  shown ${Imgarr[i].views} times.`;
+            renderElement('li', ul, display);
+        }
+    }
+}
+
+for (let i = 0; i < Imgstore.length; i++) {
+    new ImgMall(Imgstore[i]);
+}
+
+renderPics();
+renderclicks();
+
+Container.addEventListener('click', Counter);
+
+
+
+function createChart() {
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    const Imgstore = [];
+    const Votes = [];
+    for (let i = 0; i < ImgMall.all.length; i++) {
+        goatNames.push(ImgMall.all[i].name);
+        goatVotes.push(ImgMall.all[i].votes);
+    }
+
+    new Chart(ctx, {
+
+        type: 'bar',
+
+
+        data: {
+            labels: Imgstore,
+            datasets: [{
+                barPercentage: 0.5,
+
+                borderWidth: 10,
+                label: '# of votes:',
+                backgroundColor: 'rgb(100, 125, 50)',
+                borderColor: 'rgb(52, 148, 212)',
+                data: goatVotes,
+            }, ],
+        },
+
+
+        options: {},
+    });
+}
